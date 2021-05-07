bc137b289a716d3ba9db04193316b47f64082a4b
Mariana Lima <limatainer@gmail.com>
2021-05-07 18:11:27 +0100

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{' '}
+          <a
+            href="https://github.com/limatainer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mariana Cousseiro
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{' '}
+            <a
+              href="https://www.linkedin.com/in/marianacousseiro/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
